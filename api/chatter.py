8a4ea6a51ff1c66b9cf3d21003058d6ab4086c2d
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-01-04 20:11:15 +0000

    corrected the config python file

--- api/chatter.py
+++ api/chatter.py
@@ -14,7 +14,7 @@ model = ChatterboxTurboTTS.from_pretrained(device="cuda", cache_dir=cache_dir)
 print(f"Model loaded in {time.time() - start_time:.2f} seconds.")
 text = "Wow, this is absolutely amazing! [laugh] I cannot believe how incredibly awesome this is! [excited] You know, life is just so wonderfully unpredictable and full of surprises! [cough] Sometimes I just want to jump up and down with pure joy! [laugh] This whole experience has been such a delightful roller coaster of emotions and fun! [cough] I'm literally over the moon right now!"
 start_time = time.time()
-# Generate audio (requires a reference clip for voice cloning)
+
 wav = model.generate(
     text,
     top_p=0.95,
--- api/config.py
+++ api/config.py
@@ -1,28 +1,22 @@
-AUDIO_MODEL_PATH = "bosonai/higgs-audio-v2-generation-3B-base"
-AUDIO_TOKENIZER_PATH = "bosonai/higgs-audio-v2-tokenizer"
-SAMPLE_RATE = 24000
 MAX_FILE_SIZE_MB = 5
 MAX_CONCURRENT_REQUESTS = 50
-chat_ml_sample = (
-    "Generate audio following instruction.\n\n"
-    "<|scene_desc_start|>\n"
-    "A cheerful room full of energy, with a warm and friendly tone."
-    "<|scene_desc_end|>"
-)
-
-DEFAULT_SYSTEM_PROMPT = (
-    "Generate audio following instruction.\n\n"
-    "<|scene_desc_start|>\n"
-    "Audio is recorded from a quiet room.\n"
-    "<|scene_desc_end|>"
-)
 TRIAL_MODE = False
-DEFAULT_STOP_STRINGS = ["<|end_of_text|>", "<|eot_id|>"]
-TEMP_SAVE_DIR = "/tmp/higgs/"
+TEMP_SAVE_DIR = "/tmp/audio/"
 MAX_CACHE_SIZE_MB = 500 
 MAX_CACHE_FILES = 100    
 TRANSCRIBE_MODEL_SIZE = "small"
 MAX_CONCURRENT_OPERATIONS = 5 
 WORKERS = 4
 THREADS = 8
-POLLINATIONS_ENDPOINT = "https://enter.pollinations.ai/api/generate/v1/chat/completions"
+POLLINATIONS_ENDPOINT_TEXT = "https://gen.pollinations.ai/v1/chat/completions"
+paralinguistics_tags = {
+    "clear_throat": "[clear throat]",
+    "sigh": "[sigh]",
+    "shush": "[shush]",
+    "cough": "[cough]",
+    "groan": "[groan]",
+    "sniff": "[sniff]",
+    "gasp": "[gasp]",
+    "chuckle": "[chuckle]",
+    "laugh": "[laugh]"
+}
